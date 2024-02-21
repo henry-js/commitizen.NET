4274ba3e0816f3d1837c3b7cd5d22a242583055b
James Henry <jhenry@vigosoftware.com>
2024-02-21 16:23:11 +0000

    🛠 build: add minver tool and update publish

--- scripts/publish.ps1
+++ scripts/publish.ps1
@@ -1,4 +1,14 @@
 $root = Split-Path $PSScriptRoot -Parent
 Remove-Item -Path "$root\publish*" -Force -Recurse
+Remove-Item -Path "$root\packages*" -Force -Recurse
 dotnet build "$root/src/Cli" --configuration Release --output ./publish
+
 dotnet pack "$root/src/Cli" --output ./packages --no-build
+$version = dotnet minver
+$package = Get-Item "packages\*$version.nupkg"
+
+if ($null -eq $env:NUGET_API_KEY) {
+    throw "NUGET_API_KEY environment variable is not set.";
+    exit;
+}
+dotnet nuget push $package --api-key $env:NUGET_API_KEY --source https://api.nuget.org/v3/index.json
