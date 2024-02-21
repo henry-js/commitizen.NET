76c91ebcdd426a0f97327435eacdab9757ee42dc
henry-js <79054685+henry-js@users.noreply.github.com>
2024-02-21 00:58:26 +0000

    🛠 build: start setting up configuration

--- src/Cli/Program.cs
+++ src/Cli/Program.cs
@@ -1,15 +1,13 @@
-﻿using System.Text.Json;
-using RulesEngine.Models;
+﻿using commitizen.NET;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
-var workflows = JsonSerializer.Deserialize<Workflow[]>(File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory(), "workflow.json")));
-var re = new RulesEngine.RulesEngine(workflows);
-var input1 = new { country = "india", loyaltyFactor = 2, totalPurchasesToDate = 6000 };
-var input2 = new { totalOrders = 3 };
-var input3 = new { noOfVisitsPerMonth = 3 };
+HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
 
-var resultList = await re.ExecuteAllRulesAsync("Discount", input1, input2, input3);
+builder.Configuration.Sources.Clear();
 
-foreach (var result in resultList)
-{
-    Console.WriteLine($"Rule - {result.Rule.RuleName}, IsSuccess - {result.IsSuccess}");
-}
+IHostEnvironment env = builder.Environment;
+
+builder.Configuration
+    .AddJsonFile("commitTypes.json", optional: true, reloadOnChange: true);
