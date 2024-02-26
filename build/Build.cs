d8e698f5694b457a1189c3aa41c569a2c186d93d
James Henry <jhenry@vigosoftware.com>
2024-02-26 04:10:27 +0000

    🛠 build: make sure tes project is built

--- build/Build.cs
+++ build/Build.cs
@@ -105,8 +105,8 @@ class Build : NukeBuild
             ResultsDirectory.CreateOrCleanDirectory();
             Outputs = DotNetTest(_ => _
                 .SetProjectFile(TestDirectory)
-                .EnableNoBuild()
-                .EnableNoRestore()
+                // .EnableNoBuild()
+                // .EnableNoRestore()
                 .SetDataCollector("XPlat Code Coverage")
                 .SetResultsDirectory(ResultsDirectory)
                 .SetRunSetting(
