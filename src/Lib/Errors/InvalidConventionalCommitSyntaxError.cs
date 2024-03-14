525e3662cc934bf9a809e5f967537ead27c5e535
James Henry <jhenry@vigosoftware.com>
2024-03-14 13:48:34 +0000

    🐛 fix: Add severity level for InvalidSyntaxError

--- src/Lib/Errors/InvalidConventionalCommitSyntaxError.cs
+++ src/Lib/Errors/InvalidConventionalCommitSyntaxError.cs
@@ -4,17 +4,9 @@ namespace commitizen.NET.Lib;
 
 public class InvalidSyntaxError : Error
 {
-    private readonly string correctSyntax = """
-<type>[optional scope]: <description>
-
-[optional body]
-
-[optional footer(s)]
-""";
-
     public InvalidSyntaxError(string commitHeader)
         : base($"Invalid syntax: {commitHeader}.")
     {
-        Metadata.Add(nameof(correctSyntax), correctSyntax);
+        Metadata.Add("Severity", "Error");
     }
 }
